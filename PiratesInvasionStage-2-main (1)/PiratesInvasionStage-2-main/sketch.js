c8f47fc8776cb7a8940f00c1cb2e18f997acb063
aparna-wht <72865533+aparna-wht@users.noreply.github.com>
2021-06-16 23:39:35 +0530

    Add files via upload

---
+++ PiratesInvasionStage-2-main (1)/PiratesInvasionStage-2-main/sketch.js
@@ -0,0 +1,91 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var engine, world;
+var canvas, angle, tower, ground, cannon;
+
+/*var arr =[1,2,3];
+console.log(arr);
+
+var arr2 = ['name',1,true];
+console.log(arr2);
+
+var arr3 = [[1,2],[3,4],[5,6]];
+arr3.push('Aparna');
+console.log(arr3);
+arr3.pop();
+console.log(arr3);*/
+
+//console.log(arr3[0][0]);
+
+var balls =[];
+
+
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
+
+}
+
+function setup() {
+  canvas = createCanvas(1200,600);
+  engine = Engine.create();
+  world = engine.world;
+  angle = -PI / 4;
+  ground = new Ground(0, height - 1, width * 2, 1);
+  tower = new Tower(150, 350, 160, 310);
+  cannon = new Cannon(180, 110, 100, 50, angle);
+ // cannonBall = new CannonBall(cannon.x, cannon.y);
+
+}
+
+function draw() {
+  background(189);
+  image(backgroundImg, 0, 0, width, height);
+
+  
+
+  Engine.update(engine);
+  ground.display();
+  
+
+  cannon.display();
+  for(var i = 0;i<balls.length;i++)
+  {
+    showCannonBalls(balls[i],i);
+  }
+
+  tower.display();
+  //cannonBall.display()
+ 
+}
+
+function keyPressed() {
+  if (keyCode === DOWN_ARROW) {
+    var cannonBall = new CannonBall(cannon.x,cannon.y);
+    balls.push(cannonBall);
+
+    //cannonBall.shoot()
+  }
+}
+function showCannonBalls(ball,index)
+{
+  ball.display();
+  if(ball.body.position.x>=width || ball.body.position.y>=height-50)
+  {
+    Matter.World.remove(world,ball.body);
+    balls.splice(index,1);
+    
+  }
+
+}
+function keyReleased()
+{
+  if(keyCode === DOWN_ARROW)
+  {
+    balls[balls.length-1].shoot();
+
+  }
+}
